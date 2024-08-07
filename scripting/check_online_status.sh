97ec34da6d0068a3bdbbd8cc145fea06a0e1bee7
ranjithdb <ranjithdbas@gmail.com>
2024-08-07 19:16:19 +0530

    add status_file

---
+++ scripting/check_online_status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+URL="http://guvi.in"
+HTTP_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" "$URL")
+echo "HTTP status code: $HTTP_CODE"
+
+if [ "$HTTP_CODE" -eq 200 ]; then
+	echo "Success: The request was successful."
+else
+	echo "Failure: The request failed with status code $HTTP_CODE."
+fi
